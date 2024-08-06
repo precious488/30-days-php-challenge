6727c30fed789bf0e91cab28a41f084086dea1a1
precious488 <befehpreciou@gmail.com>
2024-08-06 12:25:29 +0100

    made new changes

---
+++ day1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>day1</title>
+    <style>
+        body{
+            display:flex;
+            justify-content:center;
+            align-items:center;
+        }
+        p{
+            padding-top:20%;
+            color:red;
+        }
+    </style>
+</head>
+<body>
+    <p> <?php echo "hello-world"; ?> </p>
+</body>
+</html>
+
+//jygi
