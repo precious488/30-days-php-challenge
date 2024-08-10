e12c5140b3f403b40b403046c2c665157ae573df
precious488 <befehpreciou@gmail.com>
2024-08-10 08:42:02 +0100

    Guess game

--- day5/process.php
+++ day5/process.php
@@ -1,18 +1,19 @@
 <?php
-$num =rand(1,10);
+session_start();
+$_SESSION =rand(1,100);
 
     if(isset($_POST['submit'])){
         $guest= $_POST['guest'];
 
         switch($guest){
-            case($guest < $num):
-                $result= "failed your number is lessthen the answer";
+            case($guest < $_SESSION):
+                $result= "too low";
                 break;
-            case($guest > $num):
-                $result= "your number is greater the answer";
+            case($guest > $_SESSION):
+                $result= "too high";
                 break;
-            case($guest == $num):
-                 $result= "congratulations you are correct";
+            case($guest == $_SESSION):
+                 $resul="correct!";
                  break;
             default:
                 $result="enter a number";
@@ -26,15 +27,37 @@ $num =rand(1,10);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body{
+            padding-top:200px;
+        }
+        .contaner{
+            margin:auto;
+            width: 30%;
+            padding: 10px;
+            background-color:whitesmoke;
+            box-shadow: 10px 10px 10px 10px lightblue;
+        }
+        form{
+            padding-left:15%;
+        }
+        input{
+            width: 100%;
+            padding: 12px 20px;
+           margin: 8px 0;
+           box-sizing: border-box;
+        }
+    </style>
 </head>
 <body>
     <div class="contaner">
         <form action="" method=post>
         <p>guest a number</p>
-        <input type="number" name="guest" value="">
+        <input type="number" name="guest" value=""><br>
         <button type="submit" name=submit>submit</button>
-        <p><?php echo $value?></p>
+        <p><?php echo $result?></p>
         </form>
     </div>
+
 </body>
 </html>
